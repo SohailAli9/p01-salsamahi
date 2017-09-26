83a43982cca067160242d66b939de0305013e9fb
SohailAli9 <salsamahi@mail.csuchico.edu>
2017-09-25 23:13:32 -0700

    guessing game sohail

--- guess.cpp
+++ guess.cpp
@@ -7,28 +7,28 @@ using namespace std;
 int main()
 {
 srand( time(0) );
-int eating; 
-int driving;
-driving=rand() % 10 + 1;
+int apple; 
+int pizza;
+pizza=rand() % 10 + 1;
 
-cout<<"Guess a random number between 1 and 10"<<endl;
-cin>>eating;
+cout<<"Write any random number between 1-10"<<endl;
+cin>>apple;
 
-if(eating==driving)
+if(apple==pizza)
 {
-cout<<"You, playing correctly! You win!"<<endl;
+cout<<"You were right! You win!"<<endl;
 }
-else if(eating>driving)
+else if(apple>pizza)
 {
 int C63;
-C63= eating - driving;
-cout<<"You were off by"<<C63<<" Sorry you lose"<<endl;
+C63= apple - pizza;
+cout<<"You were off by"<<C63<<" Uh oh! You lose!"<<endl;
 }
-else if(eating<driving)
+else if(apple<pizza)
 {
 int C63;
-C63= driving - eating;
-cout<<"You were off by"<<C63<<" Sorry you lose"<<endl;
+C63= pizza - apple;
+cout<<"You were off by"<<C63<<" Uh oh! You lose!"<<endl;
 }
 
 return 0;
